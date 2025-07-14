e3bb9f88bbf27d53a7f8e84b4e2ff2a74e5021ad
Javier Dominguez <jdominguez@cenamep.org.pa>
2025-07-14 08:23:39 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'devmaster',
+  webDir: 'www'
+};
+
+export default config;
